4e33f496cc4bcbda1f959e13bf76eccaef085f44
HikmahNurseha <126530455+HikmahNUrseha@users.noreply.github.com>
2023-12-08 21:50:27 +0700

    Update script.js
    perubahan jumlah order

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Toggle class active untuk shopping cart
-const shooppingCart = document.querySelector(".shopping-cart");
+const shoppingCart = document.querySelector(".shopping-cart");
 document.querySelector("#shopping-cart-button").onclick = (e) => {
-  shooppingCart.classList.toggle("active");
+  shoppingCart.classList.toggle("active");
   e.preventDefault();
 };
 
@@ -11,9 +11,37 @@ const cart = storedKeranjang;
 
 // Fungsi untuk menambahkan item ke keranjang
 function addToCart(itemName, itemPrice) {
-  const item = { name: itemName, price: itemPrice };
-  cart.push(item);
+  // Cek apakah item sudah ada di keranjang
+  const existingItem = cart.find((item) => item.name === itemName && item.price === itemPrice);
 
+  if (existingItem) {
+    // Jika item sudah ada, tingkatkan jumlahnya
+    existingItem.quantity = (existingItem.quantity || 1) + 1;
+  } else {
+    // Jika item belum ada, tambahkan ke keranjang dengan jumlah 1
+    const item = { name: itemName, price: itemPrice, quantity: 1 };
+    cart.push(item);
+  }
+
+  // Memperbarui tampilan keranjang
+  updateCartDisplay();
+
+  // Menyimpan keranjang ke dalam localStorage
+  saveCartToLocalStorage();
+}
+
+// Fungsi untuk menghapus item dari keranjang
+function removeFromCart(itemName, itemPrice) {
+  // Cari indeks item yang akan dihapus
+  const index = cart.findIndex(
+    (item) => item.name === itemName && item.price === itemPrice
+  );
+
+  // Hapus item jika ditemukan
+  if (index !== -1) {
+    cart.splice(index, 1);
+  }
+  
   // Memperbarui tampilan keranjang
   updateCartDisplay();
 
@@ -32,7 +60,7 @@ function removeFromcart(itemName, itemPrice) {
   if (index !== -1) {
     cart.splice(index, 1);
   }
-
+  
   // Memperbarui tampilan keranjang
   updateCartDisplay();
 
@@ -51,25 +79,21 @@ function updateCartDisplay() {
   // Menambahkan setiap item ke dalam elemen keranjang
   cart.forEach((item) => {
     const listItem = document.createElement("li");
-    listItem.textContent = `${item.name} - Rp ${item.price.toFixed(3)}`;
+    listItem.textContent = `${item.name} - Rp ${item.price.toFixed(3)} x ${item.quantity || 1}`;
     cartItemsElement.appendChild(listItem);
   });
 
   // Menghitung total harga dari seluruh item dalam keranjang
-  const total = cart.reduce((acc, item) => acc + item.price, 0);
+  const total = cart.reduce((acc, item) => acc + item.price * (item.quantity || 1), 0);
   cartTotalElement.textContent = total.toFixed(3);
 
   // Menyimpan total harga ke dalam localStorage
   saveTotalToLocalStorage(total.toFixed(3));
-
-  // Menetapkan nilai total harga ke dalam input dengan ID 'form-total'
-  //document.querySelector(".form-total input").value = total.toFixed(3);
 }
 
 // Fungsi untuk menyimpan keranjang ke dalam localStorage
 function saveCartToLocalStorage() {
   localStorage.setItem("keranjang", JSON.stringify(cart));
-  console.log(localStorage);
 }
 
 // Fungsi untuk menyimpan total harga ke dalam localStorage
